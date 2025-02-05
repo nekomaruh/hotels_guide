13d43537fe9c693b6c6a6fb08a0c74af1f118881
Johan Esteban <johesteb@gmail.com>
2025-02-04 21:21:13 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# hotels_guide
+ Test Flutter Developer - Guia de Moteis (Challenge)
---
+++ android/app/src/main/kotlin/com/tebium/hotelsGuide/hotels_guide/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.tebium.hotelsGuide.hotels_guide
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
