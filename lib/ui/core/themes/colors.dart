16f9f474a273c804221eb270d562fbe33e43d34f
Johan Esteban <johesteb@gmail.com>
2025-02-05 18:46:40 -0300

    Update colors.dart

--- lib/ui/core/themes/colors.dart
+++ lib/ui/core/themes/colors.dart
@@ -5,7 +5,7 @@ abstract final class AppColors {
   static const primary = Color(0xFFE4000C);
   static const unselected = Color(0xFFc2c2c2);
   static const white = Color(0xFFFFFFFF);
-  static const green = Color(0xFF50B98C);
+  static const green = Color(0xFF00BC8A);
   static const rating = Color(0xFFFFB100);
   static const primaryDark = Color(0xFFCB0000);
   static const black = Color(0xFF000000);
